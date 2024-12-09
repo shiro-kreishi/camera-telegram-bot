13b497c79a29d29b66bbeb86db9a53b196e93dfe
shiro-kreishi <vadas25@yandex.ru>
2024-12-09 13:01:57 +0500

    new camera api

--- src/bot.py
+++ src/bot.py
@@ -13,20 +13,10 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-class NoGetUpdatesFilter(logging.Filter):
-    """Фильтр для исключения логов 'getUpdates'."""
-    def filter(self, record: logging.LogRecord) -> bool:
-        return 'getUpdates' not in record.getMessage()
-
-# Применяем фильтр ко всем логгерам
-for handler in logging.getLogger().handlers:
-    handler.addFilter(NoGetUpdatesFilter())
-
 class ImageBot:
-    def __init__(self, token: str, camera_count_url: str, camera_image_url: str, user_manager: UserManager):
+    def __init__(self, token: str, api_base_url: str, user_manager: UserManager):
         self.token = token
-        self.camera_count_url = camera_count_url
-        self.camera_image_url = camera_image_url
+        self.api_base_url = api_base_url
         self.user_manager = user_manager
         self.cameras = {}
         self.app = ApplicationBuilder().token(self.token).build()
@@ -35,12 +25,13 @@ class ImageBot:
     def fetch_cameras(self) -> None:
         """Получает список камер с API и сохраняет их в self.cameras."""
         try:
-            response = requests.get(self.camera_count_url)
+            response = requests.get(f"{self.api_base_url}/cameras")
             if response.status_code == 200:
-                self.cameras = {idx: camera["index"] for idx, camera in enumerate(response.json())}
+                cameras_data = response.json()
+                self.cameras = {idx: camera for idx, camera in enumerate(cameras_data)}
                 logger.info(f"Получено {len(self.cameras)} камер.")
             else:
-                logger.error(f"Не удалось получить список камер. Код ошибки: {response.status_code}")
+                logger.error(f"Не удалось получить список камер {response.url}. Код ошибки: {response.status_code}")
         except requests.RequestException as e:
             logger.error(f"Ошибка при запросе списка камер: {e}")
 
@@ -55,30 +46,29 @@ class ImageBot:
             return
 
         logger.info(f"Пользователь {user_name} (ID: {user_id}) начал сессию.")
-        keyboard = [[f"Камера {name}"] for name in self.cameras.values()]
+        keyboard = [[camera["name"]] for camera in self.cameras.values()]
         keyboard.append(["Показать справку"])
         reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-        await update.message.reply_text("Выберите действие:", reply_markup=reply_markup)
+        await update.message.reply_text("Выберите камеру:", reply_markup=reply_markup)
 
     async def send_image(self, update: Update, camera_id: int) -> None:
         """Отправляет изображение с указанной камеры."""
+        camera = self.cameras.get(camera_id)
+        if not camera:
+            await update.message.reply_text("Указанная камера не найдена.")
+            return
+
         try:
-            response = requests.get(f'{self.camera_image_url}/{camera_id}')
-            logger.info(f"Запрос изображения с камеры {camera_id} по URL: {self.camera_image_url}/{camera_id}")
+            response = requests.get(f"{self.api_base_url}/image/{camera_id}")
             if response.status_code == 200:
                 await update.message.reply_photo(photo=response.content)
-                logger.info(f"Успешно отправлено изображение с камеры {camera_id}.")
+                logger.info(f"Отправлено изображение с камеры {camera['name']}.")
             else:
-                logger.error(f"Ошибка получения изображения с камеры {camera_id}: код {response.status_code}")
-                await update.message.reply_text(f"Не удалось получить изображение с камеры {camera_id}.")
+                logger.error(f"Ошибка получения изображения с камеры {camera['name']}: код {response.status_code}")
+                await update.message.reply_text(f"Не удалось получить изображение с камеры {camera['name']}.")
         except requests.RequestException as e:
-            logger.error(f"Ошибка при получении изображения с камеры {camera_id}: {e}")
-            await update.message.reply_text(f"Ошибка при получении изображения с камеры {camera_id}: {e}")
-
-    async def send_text(self, update: Update) -> None:
-        """Отправляет текстовое сообщение."""
-        await update.message.reply_text("Это пример текстового сообщения от бота!")
-        logger.info("Отправлено текстовое сообщение пользователю.")
+            logger.error(f"Ошибка при запросе изображения с камеры {camera_id}: {e}")
+            await update.message.reply_text(f"Ошибка при получении изображения: {e}")
 
     async def show_help(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Отправляет справку для пользователя."""
@@ -103,30 +93,6 @@ class ImageBot:
             - Показать справку - Получить справку от бота.
             """
         await update.message.reply_text(help_text)
-
-    async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        """Обрабатывает текстовые сообщения от пользователя и проверяет доступ."""
-        user_id = update.effective_user.id
-        user_name = update.effective_user.username
-
-        if not self.user_manager.is_user_allowed(user_id):
-            logger.warning(f"Пользователь {user_name} (ID: {user_id}) попытался использовать неизвестную команду.")
-            await update.message.reply_text("У вас нет доступа к этому боту.")
-            return
-
-        text = update.message.text
-        logger.info(f"Получено сообщение от {user_name} (ID: {user_id}): {text}")
-
-        for camera_id, camera_name in self.cameras.items():
-            if text == f"Камера {camera_name}":
-                await self.send_image(update, camera_id)
-                return
-
-        if text == "Показать справку":
-            await self.show_help(update, context)
-        else:
-            logger.warning(f"Неизвестная команда от пользователя {user_name} (ID: {user_id}): {text}")
-            await update.message.reply_text("Неизвестная команда. Попробуйте ещё раз.")
 
     async def add_user(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Добавление пользователя в белый список."""
@@ -172,11 +138,34 @@ class ImageBot:
         else:
             await update.message.reply_text("Белый список пуст.")
 
+    async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """Обрабатывает текстовые сообщения от пользователя."""
+        user_id = update.effective_user.id
+        user_name = update.effective_user.username
+
+        if not self.user_manager.is_user_allowed(user_id):
+            logger.warning(f"Пользователь {user_name} (ID: {user_id}) попытался отправить сообщение.")
+            await update.message.reply_text("У вас нет доступа к этому боту.")
+            return
+
+        text = update.message.text
+        logger.info(f"Получено сообщение от {user_name} (ID: {user_id}): {text}")
+
+        for camera_id, camera in self.cameras.items():
+            if text == camera["name"]:
+                await self.send_image(update, camera_id)
+                return
+
+        if text == "Показать справку":
+            await self.show_help(update, context)
+        else:
+            logger.warning(f"Неизвестная команда от пользователя {user_name} (ID: {user_id}): {text}")
+            await update.message.reply_text("Неизвестная команда. Попробуйте ещё раз.")
+
     def run(self):
         """Регистрация обработчиков и запуск бота."""
         self.fetch_cameras()
         self.app.add_handler(CommandHandler("start", self.start))
-        self.app.add_handler(CommandHandler("help", self.show_help))
         self.app.add_handler(CommandHandler("add_user", self.add_user))
         self.app.add_handler(CommandHandler("remove_user", self.remove_user))
         self.app.add_handler(CommandHandler("list_user", self.list_users))
@@ -194,8 +183,7 @@ class CameraImageBot(ImageBot):
         user_manager = UserManager(db_path="users.db", env_path=dotenv_path)
 
         token = config.telegram_token
-        camera_count_url = config.camera_count_url
-        camera_image_url = config.camera_image_url
+        api_base_url = f'http://{config.camera_ip_service}'
 
         logger.info("Инициализация CameraImageBot с параметрами из Config.")
-        super().__init__(token, camera_count_url, camera_image_url, user_manager)
+        super().__init__(token, api_base_url, user_manager)
